d89f7ae7889cb156244a09ffb5aa399658cdbd61
kolengue <kolengue@icloud.com>
2013-11-21 17:25:09 -0600

    fixed spacing in this file to 6 spaces
    fixed spacing

--- src/edu/smu/engr/softeng/horus/security/CryptionManager.java
+++ src/edu/smu/engr/softeng/horus/security/CryptionManager.java
@@ -30,90 +30,90 @@ import javax.crypto.NoSuchPaddingException;
  */
 public class CryptionManager {
 
-	private static KeyManager keyMan;
-	private DiffieHellman diffie;
+      private static KeyManager keyMan;
+      private DiffieHellman diffie;
 	
 	/**
 	 * Constructur for CryptionManager class with no inputs
 	 * @return new CryptionManager object
 	 */
-	public CryptionManager() {
-		this.diffie = new DiffieHellman();
-		this.keyMan = new KeyManager(diffie);
-	}
+      public CryptionManager() {
+            this.diffie = new DiffieHellman();
+            this.keyMan = new KeyManager(diffie);
+      }
 	
 	/**
 	 * Constructur for CryptionManager class with diffie1 inputs
 	 * @param diffie1 A DiffieHellman object
 	 * @return new CryptionManager object
 	 */
-	public CryptionManager(DiffieHellman diffie1) {
-		this.diffie = diffie1;
-		this.keyMan = new KeyManager(diffie);
-	}
+      public CryptionManager(DiffieHellman diffie1) {
+            this.diffie = diffie1;
+            this.keyMan = new KeyManager(diffie);
+      }
 	
 	/**
 	 * Encrypts input stream and returns output stream
 	 * @param input The input byte array
 	 * @return output The output byte array
 	 */
-	private static ByteArrayOutputStream encryptData(ByteArrayInputStream input)
+      private static ByteArrayOutputStream encryptData(ByteArrayInputStream input)
 			throws Exception {
-		ByteArrayOutputStream output = new ByteArrayOutputStream(
+            ByteArrayOutputStream output = new ByteArrayOutputStream(
 				input.available());
-		//Create encryption cipher
-		Cipher cipher = Cipher.getInstance("AES");
-		cipher.init(Cipher.ENCRYPT_MODE, this.keyMan.getSecret(), getSpec());
+            //Create encryption cipher
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, this.keyMan.getSecret(), getSpec());
 
-		//Attempt to encrypt the input stream
-		try (CipherOutputStream cipherOut = new CipherOutputStream(output,
+            //Attempt to encrypt the input stream
+            try (CipherOutputStream cipherOut = new CipherOutputStream(output,
 				cipher)) {
-			int AVAILABLE_INPUT = input.available();
-			byte[] array = new byte[AVAILABLE_INPUT];
-			input.read(array);
-			cipherOut.write(array);
+                  int AVAILABLE_INPUT = input.available();
+                  byte[] array = new byte[AVAILABLE_INPUT];
+                  input.read(array);
+                  cipherOut.write(array);
 		}
-		input.close();
-		return output;
-	}
+            input.close();
+            return output;
+      }
 	
 	/**
 	 * Decrypts input stream and returns output stream
 	 * @param in The input byte array
 	 * @return out The output byte array
 	 */
-	public static ByteArrayOutputStream decrypt(ByteArrayInputStream in) {
+      public static ByteArrayOutputStream decrypt(ByteArrayInputStream in) {
 		ByteArrayOutputStream out = new ByteArrayOutputStream(in.available());
-		try {
+            try {
 			//Create decryption cipher
-			Cipher decrypter = Cipher.getInstance("AES");
-			decrypter.init(Cipher.DECRYPT_MODE, keyMan.getSecret(),
+                  Cipher decrypter = Cipher.getInstance("AES");
+                  decrypter.init(Cipher.DECRYPT_MODE, keyMan.getSecret(),
 					keyMan.getSpec());
 
-			try (CipherInputStream cipherIn = new CipherInputStream(in,
+                  try (CipherInputStream cipherIn = new CipherInputStream(in,
 					decrypter)) {
-				int counter = 0;
-				for (int x = 0; x < in.available(); x++) {
-					out.write(cipherIn.read());
-				}
-			}
-			in.close();
-			out.close();
+                        int counter = 0;
+                        for (int x = 0; x < in.available(); x++) {
+                              out.write(cipherIn.read());
+                        }
+                  }
+                  in.close();
+                  out.close();
 
-		} catch (NoSuchAlgorithmException | NoSuchPaddingException
+            } catch (NoSuchAlgorithmException | NoSuchPaddingException
 				| IOException | InvalidKeyException
 				| InvalidAlgorithmParameterException ex) {
 			Logger.getLogger(CryptionManager.class.getName()).log(Level.SEVERE,
 					null, ex);
-		}
+            }
 		// Sizes match, correctly decrypted
-		if (checkSizes(in, out)) {
-			return out;
-		} else {
-			// Signifies that there was a problem
-			return null;
-		}
-	}
+            if (checkSizes(in, out)) {
+                  return out;
+            } else {
+                  // Signifies that there was a problem
+                  return null;
+            }
+      }
 	
 	/**
 	 * Verifies the input and output sizes from decryption match
@@ -121,25 +121,25 @@ public class CryptionManager {
 	 * @param out Output stream
 	 * @return sameLength Whether the length is the same or not
 	 */
-	private static boolean checkSizes(ByteArrayInputStream in,
+      private static boolean checkSizes(ByteArrayInputStream in,
 			ByteArrayOutputStream out) {
 		//Need to ensure input and output stream are same length
 		//in order to guarantee that encryption or decryption
 		//went through smoothly.
-		boolean sameLength;
-		if (in.available() == out.size()) {
-			sameLength = true;
-		} else {
-			sameLength = false;
-		}
-		return sameLength;
-	}
+            boolean sameLength;
+            if (in.available() == out.size()) {
+                  sameLength = true;
+            } else {
+                  sameLength = false;
+            }
+            return sameLength;
+      }
 	
 	/**
 	 * Placeholder
 	 */
-	private boolean validateSignature(Object one, Object two) {
-		boolean result = false;
-		return result;
-	}
+      private boolean validateSignature(Object one, Object two) {
+            boolean result = false;
+            return result;
+      }
 }
